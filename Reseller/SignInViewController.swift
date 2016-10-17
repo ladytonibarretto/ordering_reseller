be918c673d5a9fcdbd6693a2497083d81ec90d5c
ladytoni <ladytoni.barretto@gmail.com>
2016-10-17 22:22:39 +0800

    Added temp logo; Added 3 new view controllers (Sign In, Registration, View)

---
+++ Reseller/SignInViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SignInViewController.swift
+//  Reseller
+//
+//  Created by Lady Toni Barretto on 10/17/16.
+//  Copyright © 2016 Lady Toni Barretto. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class SingInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
