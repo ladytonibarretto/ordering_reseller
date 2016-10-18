b2f60c3a4d6d64c9137a3384977a75f7cc67233d
ladytoni <ladytoni.barretto@gmail.com>
2016-10-19 01:15:47 +0800

    Updated storyboard; Added registration fields

---
+++ Reseller/ShopViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ShopViewController.swift
+//  Reseller
+//
+//  Created by Lady Toni Barretto on 10/18/16.
+//  Copyright © 2016 Lady Toni Barretto. All rights reserved.
+//
+
+import UIKit
+
+class ShopViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
